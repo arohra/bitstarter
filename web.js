7a8ecbb1463410f65923ec4538e9f672a391a451
Anand Rohra <anand.s.rohra@gmail.com>
2013-07-12 05:34:53 +0000

    Updated web.js to read from index.html - 3

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var k = fs.readFile('index.html');
+  var k = fs.readFile('index.html', "utf8");
 //  console.log(k);
 //  response.send('Hello World 2!');
-  var str = k.toString();
-  response.send(str);
+//  var str = k.toString();
+  response.send(k);
 });
 
 var port = process.env.PORT || 5000;
